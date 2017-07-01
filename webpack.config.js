bf602215ccc8111e08684e3957d428af4bad1f87
Dmitry <nzh.dmitry@gmail.com>
2017-07-01 19:49:44 +0300

    feature/typescript-cfg add typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,11 @@
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/build"
-  },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".css", ".js", ".json"]
-  },
-
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      },
-
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"}
-    ]
-  },
-
-  // // When importing a module whose path matches one of the following, just
-  // // assume a corresponding global variable exists and use that instead.
-  // // This is important because it allows us to avoid bundling all of our
-  // // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //   "react": "React",
-  //   "react-dom": "ReactDOM"
-  // },
-};
+const tsc = require("typescript")
+const webpackConfig = require("fs").readFileSync("./webpack.config.ts", "utf8")
+const options = {
+  compilerOptions: {
+    target: "es5",
+    module: "commonjs",
+    allowJs: false,
+    checkJs: false
+  }
+}
+eval(tsc.transpileModule(webpackConfig, options).outputText)
---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+  devtool: 'source-map',
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.css', '.js', '.json']
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  }
+};
+
+export default config;
