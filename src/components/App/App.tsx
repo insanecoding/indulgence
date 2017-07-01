d783ee3e6795eea824362f7c2d105e32eda1c7c3
Dmitry <nzh.dmitry@gmail.com>
2017-07-01 20:34:11 +0300

    feature/set-up-webpack add airbnb tslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,10 @@
 import * as React from 'react';
 
 export interface HelloProps {
-    compiler: string;
-    framework: string;
+  compiler: string;
+  framework: string;
 }
 
 export const Hello = (props: HelloProps) => {
-    return (<h1>Hello from {props.compiler} and {props.framework}!</h1>);
+  return (<h1>Hello from {props.compiler} and {props.framework}!</h1>);
 };
