ad4b971e5f6ab811213c2b3ab2c22ec79f6baa60
Dmitry <nzh.dmitry@gmail.com>
2017-07-21 01:07:39 +0300

    feature/split-configs move tsconfig for ts-node local

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
+
 // import ts-node to transpile webpack.config.ts
 require("ts-node").register({
-  project: "./tsconfig-for-ts-node.json"
+  project: path.join(__dirname, 'tsconfig-for-ts-node.json')
 })
 // import dotenv to externalize settings to .env file
 const dotenv = require ('dotenv').config();
